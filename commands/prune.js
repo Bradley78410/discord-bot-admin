3bc0a5779d82d31a4d529cc8b4d4f9f4082e0cf3
Bradley78410 <74388491+Bradley78410@users.noreply.github.com>
2020-11-27 19:32:37 +0100

    Initial commit

---
+++ commands/prune.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: '#prune',
+    description: 'Delete messages.',
+    execute(message, args) {
+        const amount = parseInt(args[0]) + 1;
+
+        if (isNaN(amount)) {
+            return message.reply("Ce n'est pas un nombre valide !");
+        }
+        else if (amount <= 1 || amount > 100) {
+            return message.reply("Tu dois saisir un nombre compris entre 1 et 99 !");
+        }
+
+        message.channel.bulkDelete(amount).then(amount)
+            .then(message => console.log(`${message.size - 1} message supprimés.`))
+    }
+};
